41ed3de0c410677a5c0634fd1cea6c91f6ff2644
naresh <naresh@latlong.in>
2019-11-10 13:49:05 +0530

    Updates

---
+++ precache-manifest.58895c1c12249d99db0b4185f8a0ac27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e851eecf901cb676b1778a42a46e5ff7",
+    "url": "/react-shooping-app/index.html"
+  },
+  {
+    "revision": "2f80165ff0d4e6ec3f92",
+    "url": "/react-shooping-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "9b26c8a837a2b0c713ec",
+    "url": "/react-shooping-app/static/js/2.29eb919f.chunk.js"
+  },
+  {
+    "revision": "2f80165ff0d4e6ec3f92",
+    "url": "/react-shooping-app/static/js/main.2f80d82d.chunk.js"
+  },
+  {
+    "revision": "4d1a3c777888a7379b5d",
+    "url": "/react-shooping-app/static/js/runtime-main.ce237fbb.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-shooping-app/static/media/logo.25bf045c.svg"
+  }
+]);
